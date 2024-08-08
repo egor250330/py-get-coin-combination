2250f1ce37bb0519a8ac39c1019302fc4657cd43
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-08-08 21:16:54 +0300

    first commit

--- app/test_main.py
+++ app/test_main.py
@@ -12,5 +12,5 @@ from app.main import get_coin_combination
         (50, [0, 0, 0, 2]),
     ]
 )
-def test_get_coin_combination(centc, excepted):
+def test_get_coin_combination(centc: int, excepted: list) -> None:
     assert get_coin_combination(centc) == excepted
